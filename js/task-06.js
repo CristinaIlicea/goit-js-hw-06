54d70afaa0e3f4cb07adddf10524bcf1cdb4e1c0
CristinaIlicea <ilicea.cristina@yahoo.com>
2024-04-07 19:27:15 +0300

    update task 6

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const textInput = document.querySelector(".validation-input");
+const dataLength = document.querySelector(".data-length");
+
+function validateForm() {
+    let form = document.getElementById('validation-input');
+    for (var i=0; i< validation-input.length; i++) {
+        if(validation-input.length <= dataLength) {
+            alert("Please enter 6 symbols");
+            return false;
+        }
+    }};
+textInput.addEventListener(textInput.focus, validateForm);
