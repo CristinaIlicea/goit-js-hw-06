48e59eae64c2032515e29a0965991000bbe47d4b
CristinaIlicea <ilicea.cristina@yahoo.com>
2024-04-10 19:59:13 +0300

    task 7 done

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,8 @@
-var slider = document.getElementById('font-size-control');
-const text = document.getElementById('.text')
+var slider = document.getElementById("font-size-control");
 
-slider.addEventListener('input', function() {
+slider.addEventListener("input", function() {
     var size = slider.value;
+    let text = document.getElementById("text")
     // this sets the body's font size property, but you can set whatever you need to
-    text.style.fontSize = size + "px";
+   text.style.fontSize = size + "px";
 });
